53c4c5d4fdc8f9ef6416d1ee41147e2a6ed5d1c9
bmw-adame <baboraka@gasos-ro.cz>
2025-10-16 15:36:50 +0200

    Kube update

--- kubernetes/example.nix
+++ kubernetes/example.nix
@@ -1,13 +1,20 @@
+# This file should just be a NixOS-style module.
+# Kubenix's `evalModules` function will automatically pass the necessary
+# arguments like `kubenix`, `pkgs`, `config`, etc.
 { kubenix, ... }: {
   imports = [
     kubenix.modules.k8s
   ];
 
   # Define your Kubernetes resources here.
-  kubernetes.resources.pods.example.spec.containers.nginx = {
-    image = "nginx:latest";
-    ports = [
-      { containerPort = 80; }
-    ];
+  kubernetes.resources.pods.example = {
+    spec.containers.nginx = {
+      image = "nginx:latest";
+
+      # Always include ports to avoid 'protocol missing' errors
+      ports = [
+        { containerPort = 80; protocol = "TCP"; }
+      ];
+    };
   };
 }
