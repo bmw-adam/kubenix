18da1412a56cef74dde4ab3bdb261b2d7c1ba132
bmw-adame <baboraka@gasos-ro.cz>
2025-10-16 15:27:59 +0200

    Container Port Test

--- kubernetes/example.nix
+++ kubernetes/example.nix
@@ -1,11 +1,13 @@
-# This file should just be a NixOS-style module.
-# Kubenix's `evalModules` function will automatically pass the necessary
-# arguments like `kubenix`, `pkgs`, `config`, etc.
 { kubenix, ... }: {
   imports = [
     kubenix.modules.k8s
   ];
 
   # Define your Kubernetes resources here.
-  kubernetes.resources.pods.example.spec.containers.nginx.image = "nginx:latest";
+  kubernetes.resources.pods.example.spec.containers.nginx = {
+    image = "nginx:latest";
+    ports = [
+      { containerPort = 80; }
+    ];
+  };
 }
